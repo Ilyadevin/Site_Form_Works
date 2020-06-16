a43f3e677beb507d531255982a227dcd04d5d95d
Ilya Erlingas <Aligruit@gmail.com>
2020-06-16 17:10:48 +0300

    new form

--- car_admin/app/forms.py
+++ car_admin/app/forms.py
@@ -2,8 +2,8 @@ from django import forms
 
 
 class ReviewAdminForm(forms.ModelForm):
-    brand = forms.CharField(label='Brand', max_length=15)
-    mark = forms.CharField(label='Mark', max_length=64)
+    brand = forms.CharField(label='brand', max_length=15)
+    mark = forms.CharField(label='model', max_length=64)
     review = forms.CharField(label='Review', )
 
     class Meta:
